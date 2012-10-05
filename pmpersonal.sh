c7567537eb8d008f7360df4c10992076a23cd23d
Taro Sato <okomestudio@gmail.com>
2012-10-04 20:33:13 -0700

    updated to not use tp_smapi

--- pmpersonal.sh
+++ pmpersonal.sh
@@ -1,46 +1,47 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ##############################################################################
 # This is a script to run when AC/battery switches.  Ideally used
 # within the power management configuration setting on KDE.  Requires
 # tp_smapi to be loaded for the detection of AC/battery.
-
+  
 USERNAME=taro
 LATITUDE=37.78
 LONGITUDE=-122.42
-
-
+  
+  
 ##############################################################################
-# DO NOT MODIFY BELOW 
-
-if [ ! -f /sys/devices/platform/smapi/ac_connected ]; then
+# DO NOT MODIFY BELOW
+ 
+statuspath="/sys/class/power_supply/AC/online"
+  
+if [ ! -f "$statuspath" ]; then
     exit 0
 fi
-
-TAG=pmpersonal
-STATUS=`cat /sys/devices/platform/smapi/ac_connected`
-
-if [ "$STATUS" = "1" ]; then
-    logger -t $TAG "on ac"
-
-    logger -t $TAG "start xflux"
+  
+tag=pmpersonal
+status=`cat "$statuspath"`
+ 
+if [ "$status" = "1" ]; then
+    logger -t $tag "on ac"
+  
+    logger -t $tag "start xflux"
     for i in `pgrep ^xflux$` ; do kill -9 $i ; done
     xflux -l $LATITUDE -g $LONGITUDE
-
-    logger -t $TAG "start dropbox"
-    dropbox stop
+  
+    logger -t $tag "start dropbox"
     dropbox start
-
-    logger -t $TAG "enable system notification"
+  
+    logger -t $tag "enable system notification"
     kwriteconfig --file="/home/$USERNAME/.kde/share/config/knotifyrc" --group=Sounds --key="No sound" false
 else
-    logger -t $TAG "on battery"
-
-    logger -t $TAG "stop xflux"
+    logger -t $tag "on battery"
+  
+    logger -t $tag "stop xflux"
     for i in `pgrep ^xflux$` ; do kill -9 $i ; done
-
-    logger -t $TAG "stop dropbox"
+  
+    logger -t $tag "stop dropbox"
     dropbox stop
-
-    logger -t $TAG "disable system notification"
+  
+    logger -t $tag "disable system notification"
     kwriteconfig --file="/home/$USERNAME/.kde/share/config/knotifyrc" --group=Sounds --key="No sound" true
 fi
